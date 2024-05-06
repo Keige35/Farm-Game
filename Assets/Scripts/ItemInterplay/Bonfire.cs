86d2c1e83b26723fee0da4f8d42be4c38bf2275d
Keige125 <NiskoromniiNL@outlook.com>
2024-05-06 18:15:19 +0300

    HomeWork

---
+++ Assets/Scripts/ItemInterplay/Bonfire.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Bonfire : InterplayObject
+{
+    [SerializeField] private ParticleSystem particleSystemBonfire;
+    public override void IterplayObject(CharacterInvetory characterInvetory)
+    {
+        var item = characterInvetory.GetHandItemByType(HandItemType.FullBucket);
+        if (item)
+        {
+            item.ReturnToPool();
+            var newBucket = ItemSpawner.Instance.GetHandItemByType(HandItemType.Bucket);
+            characterInvetory.AddNewHandItem(newBucket);
+            ChokeFire();
+        }
+        else
+        {
+            LightFire();
+        }
+    }
+
+    void LightFire()
+    {
+        particleSystemBonfire.gameObject.SetActive(true);
+    }
+
+    void ChokeFire()
+    {
+        particleSystemBonfire.gameObject.SetActive(false);
+    }
+}
